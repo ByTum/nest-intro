fb24d10b45f67334680f8ba24e886844802190a7
Tum <tum-ok@msn.com>
2024-09-01 00:01:17 +0700

    Additional Request Components, Headers, Ip Decorators

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,32 @@
-import { Controller, Get, Param, Post, Query, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Body,
+  Headers,
+  Ip,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id/:optional?')
-  public getUsers(@Param() params: any, @Query() query: any) {
-    console.log(params);
-    console.log(query);
+  public getUsers(@Param('id') id: any, @Query('limit') limit: any) {
+    console.log(id);
+    console.log(limit);
     return 'You send a get request to users endpoint';
   }
 
   @Post()
-  public createUsers(@Body() request: any) {
+  public createUsers(
+    @Body() request: any,
+    @Headers() headers: any,
+    @Ip() ip: any,
+  ) {
     console.log(request);
+    console.log(headers);
+    console.log(ip);
     return 'You send a post request to users endpoint';
   }
 }
