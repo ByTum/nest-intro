f54f593c0fc943983f5e185dd809f24c382dd972
Tum <tum-ok@msn.com>
2024-09-02 14:50:48 +0700

    Use Users Service Inside Posts Service | Inter modular dependencies

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/providers/users.service';
 
 @Injectable()
 export class PostsService {
+  constructor(private readonly usersService: UsersService) {}
   public findAll(userId: string) {
+    const user = this.usersService.findOneById(userId);
     return [
       {
+        user: user,
         title: 'title',
         content: 'content',
       },
       {
+        user: user,
         title: 'title 2',
         content: 'content 2',
       },
