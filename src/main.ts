8ad7c1cc84e495f1b246c2c3d18f2176afe38f15
Tum <tum-ok@msn.com>
2024-09-07 00:05:41 +0700

    Adding Configuration Methods to Swagger, add ApiTags

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,17 @@ async function bootstrap() {
   );
 
   // swagger configuration
-  const config = new DocumentBuilder().setVersion('1.0').build();
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Intro api-docement')
+    .setDescription('Use the base API URL as http://localhost:3000')
+    .setTermsOfService('http://localhost:3000/terms-of-service')
+    .setLicense(
+      'MIT License',
+      'https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt',
+    )
+    .addServer('http://localhost:3000')
+    .setVersion('1.0')
+    .build();
 
   // Instantital Document
   const document = SwaggerModule.createDocument(app, config);
