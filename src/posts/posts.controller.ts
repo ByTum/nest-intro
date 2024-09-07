66730d80b2caed2abc49c54264553152b16d2483
Tum <tum-ok@msn.com>
2024-09-08 00:13:45 +0700

    Mapped Types Using Swagger | use when create Update Dto

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreatePostDto } from './dtos/create-post.dto';
+import { PatchPostDto } from './dtos/patch-post.dto';
 
 @Controller('posts')
 @ApiTags('Posts')
@@ -13,8 +14,27 @@ export class PostsController {
     return this.postsService.findAll(userId);
   }
 
+  @ApiOperation({
+    summary: 'Created a new blog post',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'You get a 201 Response if your post is created successfully',
+  })
   @Post()
   public createPost(@Body() createPostDto: CreatePostDto) {
     console.log(createPostDto);
+  }
+
+  @ApiOperation({
+    summary: 'Updates as existing blog post',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'a 200 response if the post is updated successfully',
+  })
+  @Patch()
+  public updatePost(@Body() patchPostDto: PatchPostDto) {
+    console.log(patchPostDto);
   }
 }
