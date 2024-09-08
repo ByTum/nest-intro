4da7aab8a81f5c268c36f744f0f953408e4150e4
Tum <tum-ok@msn.com>
2024-09-08 21:31:18 +0700

    Creating the Tags Entity

---
+++ src/tags/tag.entity.ts
@@ -0,0 +1,59 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Tag {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 256,
+    nullable: false,
+    unique: true,
+  })
+  name: string;
+
+  @Column({
+    type: 'varchar',
+    length: 512,
+    nullable: false,
+    unique: true,
+  })
+  slug: string;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  descriptsion?: string;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  schema?: string;
+
+  @Column({
+    type: 'varchar',
+    length: 1024,
+    nullable: true,
+  })
+  featureImageUrl?: string;
+  //https://orkhan.gitbook.io/typeorm/docs/decorator-reference
+
+  @CreateDateColumn()
+  createAt: Date;
+
+  @UpdateDateColumn()
+  updateAt: Date;
+
+  @DeleteDateColumn()
+  deleteAt: Date;
+}
