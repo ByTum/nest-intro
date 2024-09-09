6bc189f5a4683123797b26530622fbdd320910e2
Tum <tum-ok@msn.com>
2024-09-09 13:09:15 +0700

    Creating Post With Relationships | create post with metaOptions relationship

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/providers/users.service';
+import { CreatePostDto } from '../dtos/create-post.dto';
+import { Repository } from 'typeorm';
+import { Post } from '../post.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MetaOption } from 'src/meta-options/meta-option.entity';
 
 @Injectable()
 export class PostsService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    /**
+     * Injecting UserService
+     * */
+    private readonly usersService: UsersService,
+    /**
+     * Inject postsRepository
+     */
+    @InjectRepository(Post)
+    private readonly postsRepository: Repository<Post>,
+    /**
+     * Inject metaOptionsRepository
+     */
+    @InjectRepository(MetaOption)
+    public readonly metaOptionsRepository: Repository<MetaOption>,
+  ) {}
+
+  /**
+   * creting new posts
+   */
+  public async create(createPostDto: CreatePostDto) {
+    // Create metaOptions
+    const metaOptions = createPostDto.metaOptions
+      ? this.metaOptionsRepository.create(createPostDto.metaOptions)
+      : null;
+    if (metaOptions) {
+      await this.metaOptionsRepository.save(metaOptions);
+    }
+    // Create post
+    const post = this.postsRepository.create(createPostDto);
+    // Add metaOptions to post
+    if (metaOptions) {
+      post.metaOptions = metaOptions;
+    }
+    // return the post
+    return await this.postsRepository.save(post);
+  }
+
   public findAll(userId: string) {
     const user = this.usersService.findOneById(userId);
     return [
