b5d37479feed3e72182b5fe76856598ac8b59901
Tum <tum-ok@msn.com>
2024-09-22 11:18:30 +0700

    Exception Handling Model Constraints | users service

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,5 +1,10 @@
 import { CreateUserDto } from './../dtos/create-user.dto';
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  RequestTimeoutException,
+} from '@nestjs/common';
 import { GetUsersParamDto } from '../dtos/get-users-param.dto';
 import { Repository } from 'typeorm';
 import { User } from '../user.entity';
@@ -28,13 +33,31 @@ export class UsersService {
   ) {}
 
   public async createUser(createUserDto: CreateUserDto) {
+    let existingUser = undefined;
     // check email is exist with same email
-    const existingUser = await this.usersRepository.findOne({
-      where: {
-        email: createUserDto.email,
-      },
-    });
+    try {
+      existingUser = await this.usersRepository.findOne({
+        where: {
+          email: createUserDto.email,
+        },
+      });
+    } catch (error) {
+      // might save the details of the excenption
+      // Information which is sensitive
+      throw new RequestTimeoutException(
+        'Unable to process your request at the moment please try later',
+        {
+          description: 'Error connecting to the database',
+        },
+      );
+    }
+
     // Handle exception
+    if (existingUser) {
+      throw new BadRequestException(
+        'The use already exists, please check your email.',
+      );
+    }
     // Create a new user
     let newUser = this.usersRepository.create(createUserDto);
     newUser = await this.usersRepository.save(newUser);
