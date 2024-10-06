427644c0f3baaabc1aa5a0f560a6679167f48660
Tum <tum-ok@msn.com>
2024-10-06 21:40:53 +0700

    Creating AccessTokenGuard and  Completing AccessTokenGuard Implementation

---
+++ src/auth/guards/access-token/access-token.guard.ts
@@ -0,0 +1,54 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Observable } from 'rxjs';
+import jwtConfig from 'src/auth/config/jwt.config';
+import { Request } from 'express';
+import { REQUEST_USER_KEY } from 'src/auth/constants/auth.constants';
+@Injectable()
+export class AccessTokenGuard implements CanActivate {
+  constructor(
+    /**
+     * Inject jwtService
+     */
+    private readonly jwtService: JwtService,
+    /**
+     * Inject jwtConfiguration
+     */
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    // extract the request from the exucution context
+    const request = context.switchToHttp().getRequest();
+    // extract the token from header
+    const token = this.extractRequestFromHeader(request);
+    // validate the token
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(
+        token,
+        this.jwtConfiguration,
+      );
+      request[REQUEST_USER_KEY] = payload;
+      console.log(payload);
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractRequestFromHeader(request: Request): string | undefined {
+    const [_, token] = request.headers.authorization?.split(' ') ?? [];
+    return token;
+  }
+}
