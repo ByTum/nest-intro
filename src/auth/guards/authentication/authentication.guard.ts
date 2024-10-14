aa19547eb9b76e526a40582334d6c9e699d6e4e2
Tum <tum-ok@msn.com>
2024-10-14 23:51:35 +0700

    AuthenticationGuard Implemenation

--- src/auth/guards/authentication/authentication.guard.ts
+++ src/auth/guards/authentication/authentication.guard.ts
@@ -1,8 +1,14 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { AccessTokenGuard } from '../access-token/access-token.guard';
 import { Reflector } from '@nestjs/core';
 import { AuthType } from 'src/auth/enums/auth-type.enum';
+import { AUTH_TYPE_KEY } from 'src/auth/constants/auth.constants';
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
@@ -20,10 +26,35 @@ export class AuthenticationGuard implements CanActivate {
     private readonly reflector: Reflector,
     private readonly accessTokenGuard: AccessTokenGuard,
   ) {}
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    console.log(this.authTypeGuardMap);
-    return true;
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    // authTypes from reflector
+    const authTypes = this.reflector.getAllAndOverride(AUTH_TYPE_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]) ?? [AuthenticationGuard.defaultAuthType];
+    console.log('authTypes', authTypes);
+
+    // array of guards
+    const guards = authTypes.map((type) => this.authTypeGuardMap[type]).flat();
+    console.log('guards', guards);
+
+    // Default error
+    const error = new UnauthorizedException();
+
+    // Loop guards canActivate
+    for (const instance of guards) {
+      console.log('instance', instance);
+      const canActivate = await Promise.resolve(
+        instance.canActivate(context),
+      ).catch((err) => {
+        error: err;
+      });
+      console.log('canActivate', canActivate);
+      if (canActivate) {
+        return true;
+      }
+    }
+
+    throw error;
   }
 }
