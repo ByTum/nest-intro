d0cdac4703449dcdd259ce16db59cad4d748445a
Tum <tum-ok@msn.com>
2024-10-15 11:56:20 +0700

    Generate Tokens Provider | within signIn provider

---
+++ src/auth/providers/generate-tokens.provider.ts
@@ -0,0 +1,34 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import jwtConfig from '../config/jwt.config';
+
+@Injectable()
+export class GenerateTokensProvider {
+  constructor(
+    /**
+     * Inject jwtService
+     */
+    private readonly jwtService: JwtService,
+    /**
+     * Inject jwtConfiguration
+     */
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+
+  public async signToken<T>(userId: number, expiresIn: number, payload?: T) {
+    return await this.jwtService.signAsync(
+      {
+        sub: userId,
+        ...payload,
+      },
+      {
+        audience: this.jwtConfiguration.audience,
+        issuer: this.jwtConfiguration.issuer,
+        secret: this.jwtConfiguration.secret,
+        expiresIn,
+      },
+    );
+  }
+}
