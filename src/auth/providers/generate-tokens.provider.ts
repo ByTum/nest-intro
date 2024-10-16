3079af2e0be8abc97b57c2b386c02f96af7732c9
Tum <tum-ok@msn.com>
2024-10-16 14:45:09 +0700

    Generate Tokens Method | create generateTokens return accessToken and refreshToken

--- src/auth/providers/generate-tokens.provider.ts
+++ src/auth/providers/generate-tokens.provider.ts
@@ -2,6 +2,8 @@ import { Inject, Injectable } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import jwtConfig from '../config/jwt.config';
+import { User } from 'src/users/user.entity';
+import { ActiveUserData } from '../interfaces/active-user-data.interface';
 
 @Injectable()
 export class GenerateTokensProvider {
@@ -30,5 +32,26 @@ export class GenerateTokensProvider {
         expiresIn,
       },
     );
+  }
+
+  public async generateTokens(user: User) {
+    const [accessToken, refreshToken] = await Promise.all([
+      // Generate access token
+      this.signToken<Partial<ActiveUserData>>(
+        user.id,
+        this.jwtConfiguration.accessTokenTtl,
+        {
+          email: user.email,
+        },
+      ),
+
+      // Generate refresh token
+      this.signToken(user.id, this.jwtConfiguration.refreshTokenTtl),
+    ]);
+
+    return {
+      accessToken,
+      refreshToken,
+    };
   }
 }
